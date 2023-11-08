55d3bf2d589982b3bf6fdf7645b7b7262d872b0b
Cherokee Walters <cherokeewalters162@gmail.com>
2023-11-08 16:14:07 -0500

    Restructured modules

--- src/card/mod.rs
+++ src/card/mod.rs
@@ -1,93 +1,90 @@
 mod test;
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
 
-mod card {
-    use rand::{
-        distributions::{Distribution, Standard},
-        Rng,
-    };
+#[derive(PartialEq, Debug)]
+pub struct Card {
+    suit: Suit,
+    rank: Rank,
+}
 
-    #[derive(PartialEq, Debug)]
-    pub struct Card {
-        suit: Suit,
-        rank: Rank,
+impl Card {
+    pub fn new(suit: Suit, rank: Rank) -> Self {
+        Self { suit, rank }
     }
 
-    impl Card {
-        pub fn new(suit: Suit, rank: Rank) -> Self {
-            Self { suit, rank }
-        }
-
-        pub fn random() -> Self {
-            Self {
-                suit: rand::random(),
-                rank: rand::random(),
-            }
-        }
-
-        pub fn get_suit(&self) -> Suit {
-            self.suit
-        }
-
-        pub fn get_rank(&self) -> Rank {
-            self.rank
+    pub fn random() -> Self {
+        Self {
+            suit: rand::random(),
+            rank: rand::random(),
         }
     }
 
-    #[derive(PartialEq, Debug, Copy, Clone)]
-    pub enum Suit {
-        Diamonds,
-        Spades,
-        Clovers,
-        Hearts,
+    pub fn get_suit(&self) -> Suit {
+        self.suit
     }
 
-    impl Distribution<Suit> for Standard {
-        fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Suit {
-            match rng.gen_range(0..=3) {
-                0 => Suit::Spades,
-                1 => Suit::Diamonds,
-                2 => Suit::Hearts,
-                3 => Suit::Clovers,
-                _ => Suit::Spades,
-            }
+    pub fn get_rank(&self) -> Rank {
+        self.rank
+    }
+}
+
+#[derive(PartialEq, Debug, Copy, Clone)]
+pub enum Suit {
+    Diamonds,
+    Spades,
+    Clovers,
+    Hearts,
+}
+
+impl Distribution<Suit> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Suit {
+        match rng.gen_range(0..=3) {
+            0 => Suit::Spades,
+            1 => Suit::Diamonds,
+            2 => Suit::Hearts,
+            3 => Suit::Clovers,
+            _ => Suit::Spades,
         }
     }
+}
 
-    #[derive(PartialEq, Debug, Copy, Clone)]
-    pub enum Rank {
-        Ace,
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Nine,
-        Ten,
-        Jack,
-        Queen,
-        King,
-    }
+#[derive(PartialEq, Debug, Copy, Clone)]
+pub enum Rank {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+}
 
-    impl Distribution<Rank> for Standard {
-        fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Rank {
-            match rng.gen_range(0..=13) {
-                0 => Rank::Ace,
-                1 => Rank::Two,
-                2 => Rank::Three,
-                3 => Rank::Four,
-                4 => Rank::Five,
-                5 => Rank::Six,
-                7 => Rank::Seven,
-                8 => Rank::Eight,
-                9 => Rank::Nine,
-                10 => Rank::Ten,
-                11 => Rank::Jack,
-                12 => Rank::Queen,
-                13 => Rank::King,
-                _ => Rank::Ace,
-            }
+impl Distribution<Rank> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Rank {
+        match rng.gen_range(0..=13) {
+            0 => Rank::Ace,
+            1 => Rank::Two,
+            2 => Rank::Three,
+            3 => Rank::Four,
+            4 => Rank::Five,
+            5 => Rank::Six,
+            7 => Rank::Seven,
+            8 => Rank::Eight,
+            9 => Rank::Nine,
+            10 => Rank::Ten,
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => Rank::Ace,
         }
     }
 }
--- src/card/test.rs
+++ src/card/test.rs
@@ -1,94 +1,92 @@
 #[cfg(test)]
-mod card_tests {
-    use crate::card::card::{Card, Rank, Rank::*, Suit, Suit::*};
+use crate::card::card::{Card, Rank, Rank::*, Suit, Suit::*};
 
-    #[test]
-    fn create_new_card() {
-        let _card = Card::new(Diamonds, Ace);
-    }
+#[test]
+fn create_new_card() {
+    let _card = Card::new(Diamonds, Ace);
+}
 
-    #[test]
-    fn create_random_card() {
-        let _card = Card::random();
-    }
+#[test]
+fn create_random_card() {
+    let _card = Card::random();
+}
 
-    #[test]
-    fn get_card_suit() {
-        let card = Card::random();
-        let _suit = card.get_suit();
-    }
+#[test]
+fn get_card_suit() {
+    let card = Card::random();
+    let _suit = card.get_suit();
+}
 
-    #[test]
-    fn get_card_suit_matching_constructor() {
-        let card = Card::new(Spades, Ace);
-        let suit = card.get_suit();
-        assert_eq!(suit, Spades);
-    }
+#[test]
+fn get_card_suit_matching_constructor() {
+    let card = Card::new(Spades, Ace);
+    let suit = card.get_suit();
+    assert_eq!(suit, Spades);
+}
 
-    #[test]
-    fn create_spades_card() {
-        let card = Card::new(Spades, Ace);
-        assert_eq!(card.get_suit(), Spades)
-    }
+#[test]
+fn create_spades_card() {
+    let card = Card::new(Spades, Ace);
+    assert_eq!(card.get_suit(), Spades)
+}
 
-    #[test]
-    fn create_clover_card() {
-        let card = Card::new(Clovers, Ace);
-        assert_eq!(card.get_suit(), Clovers)
-    }
+#[test]
+fn create_clover_card() {
+    let card = Card::new(Clovers, Ace);
+    assert_eq!(card.get_suit(), Clovers)
+}
 
-    #[test]
-    fn create_hearts_card() {
-        let card = Card::new(Hearts, Ace);
-        assert_eq!(card.get_suit(), Hearts)
-    }
+#[test]
+fn create_hearts_card() {
+    let card = Card::new(Hearts, Ace);
+    assert_eq!(card.get_suit(), Hearts)
+}
 
-    #[test]
-    fn create_diamonds_card() {
-        let card = Card::new(Diamonds, Ace);
-        assert_eq!(card.get_suit(), Diamonds)
-    }
+#[test]
+fn create_diamonds_card() {
+    let card = Card::new(Diamonds, Ace);
+    assert_eq!(card.get_suit(), Diamonds)
+}
 
-    #[test]
-    fn get_card_rank() {
-        let card = Card::random();
-        let _rank = card.get_rank();
-    }
+#[test]
+fn get_card_rank() {
+    let card = Card::random();
+    let _rank = card.get_rank();
+}
 
-    #[test]
-    fn rank_matches_constructor() {
-        let card = Card::new(Spades, Ace);
-        let rank = card.get_rank();
-        assert_eq!(rank, Ace)
-    }
-    #[test]
-    fn card_can_be_all_ranks() {
-        impl Rank {
-            fn iterate() -> impl Iterator<Item = Rank> {
-                [
-                    Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King,
-                ]
-                .iter()
-                .copied()
-            }
-        }
-
-        for rank in Rank::iterate() {
-            let card = Card::new(Spades, rank);
-            assert_eq!(card, Card::new(Spades, rank));
+#[test]
+fn rank_matches_constructor() {
+    let card = Card::new(Spades, Ace);
+    let rank = card.get_rank();
+    assert_eq!(rank, Ace)
+}
+#[test]
+fn card_can_be_all_ranks() {
+    impl Rank {
+        fn iterate() -> impl Iterator<Item = Rank> {
+            [
+                Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King,
+            ]
+            .iter()
+            .copied()
         }
     }
 
-    #[test]
-    fn card_can_be_all_suits() {
-        impl Suit {
-            fn iterate() -> impl Iterator<Item = Suit> {
-                [Diamonds, Hearts, Spades, Clovers].iter().copied()
-            }
+    for rank in Rank::iterate() {
+        let card = Card::new(Spades, rank);
+        assert_eq!(card, Card::new(Spades, rank));
+    }
+}
+
+#[test]
+fn card_can_be_all_suits() {
+    impl Suit {
+        fn iterate() -> impl Iterator<Item = Suit> {
+            [Diamonds, Hearts, Spades, Clovers].iter().copied()
         }
-        for suit in Suit::iterate() {
-            let card = Card::new(suit, Ace);
-            assert_eq!(card, Card::new(suit, Ace))
-        }
+    }
+    for suit in Suit::iterate() {
+        let card = Card::new(suit, Ace);
+        assert_eq!(card, Card::new(suit, Ace))
     }
 }
