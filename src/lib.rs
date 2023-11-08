4976029314d5037ea40e449b4244fb4fff5b725c
Cherokee Walters <cherokeewalters162@gmail.com>
2023-11-07 20:06:41 -0500

    Added get_suit

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
+#![allow(dead_code)]
 struct Card {
-
+    suit: &'static str,
 }
 
 impl Card {
-    fn new() -> Card {
-        Card {}
+    fn new() -> Self {
+        Self {
+            suit: "Diamond"
+        }
+    }
+
+    fn get_suit(&self) -> &'static str {
+        self.suit
     }
 }
 
@@ -14,6 +21,13 @@ mod tests {
 
     #[test]
     fn create_new_card() {
+        let _card = Card::new();
+    }
+
+    #[test]
+    fn get_card_suit() {
         let card = Card::new();
+        let suit = card.get_suit();
+        assert_eq!(suit, "Diamond");
     }
 }
